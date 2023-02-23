b37c6d9dba04d96dc38998e65b907296f4f03846
Gilang Ramadhan <gilang.ramadhan011297+github@gmail.com>
2023-02-23 09:49:30 +0700

    JPR-3 Progress

--- projects/server/src/controller/userController.js
+++ projects/server/src/controller/userController.js
@@ -99,6 +99,103 @@ module.exports = {
         }
     },
 
-
+    activation: async(req, res) => {
+        try {
+      
+            const {id, otp} = req.body;
+            console.log(otp)
+            console.log(id)
+      
+            const findUser = await users.findOne({
+                where: {
+                    id: id
+                }
+            })
+      
+            
+            if(!findUser){
+               return res.status(400).send({
+                    isError: true,
+                    message: 'User Not Found',
+                    data: null
+                })
+            }
+      
+      
+            if(findUser.status !== "unconfirmed"){
+               return res.status(400).send({
+                    isError: true,
+                    message: 'User has already been confirmed',
+                    data: null
+                })
+            }
+            
+            
+            
+            if(!findUser.dataValues.otp_code === otp){
+               return res.status(400).send({
+                    isError: true,
+                    message: 'Invalid OTP',
+                    data: null
+                })
+            }
+            console.log(findUser.dataValues.otp_code )
+            console.log(otp)
+      
+            const otp_created_at = new Date(findUser.otp_created_at);
+            const now = new Date();
+            const diffInMs = now - otp_created_at;
+            const diffInDays = diffInMs / (24 * 60 * 60 * 1000);
+        
+            console.log('tes')
+            if(diffInDays > 1){
+               return res.status(400).send({
+                    isError: true,
+                    message: 'OTP has expired',
+                    data: null
+                })
+            }
+            
+            findUser.status = "confirmed";
+            console.log(findUser)
+            await findUser.save();
+            if(findUser.status = "confirmed"){
+               return res.status(200).send({
+                    isError: false,
+                    message: 'User Validate Success',
+                    data: null
+                })
+            }
+            
+            return findUser;
+            
+        } catch (error) {
+           return res.status(404).send({
+                isError: true,
+                message: error.message,
+                data: null
+            })
+        }
+      },
+      
+    //   resendOtp: async() => {
+    //     try {
+    //         const user = await users.findOne({where: {id: req.params.id}})
+    //         if(!user){
+    //            return res.status(404).send({
+    //                isError: true,
+    //                message: "User Not Found",
+    //                data: null
+    //            })
+    //         }
+      
+    //         const otp = Math.floor(10000 + Math.random() * 9000);   
+    //         users.otp_code = newOtp;
+    //         users.otp_created_at = newDat();
+            
+    //     } catch (error) {
+            
+    //     }
+    //   }
        
 }
