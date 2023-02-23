a89ce91a5b476c2b60aaf96418866111f077838e
ginanjarjtp <ginanjarjtp@gmail.com>
2023-02-23 17:23:15 +0700

    Ginanjar-JPR-1-landing Page Done

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,25 +1,24 @@
-import axios from "axios";
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Type from "./components/Type";
+// import Carousel from "./components/carousel";
+import Rentals from "./components/Rentals";
+import Footer from "./components/Footer"
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      );
-      setMessage(data?.message || "");
-    })();
-  }, []);
   return (
-    <div className="App">
-      <header className="App-header my-bg-main text-xs">
-       tester tailwind
-       tes baru
-      </header>
+    <div className="">
+      {/* Navbar */}
+      <Navbar />
+      {/* Carousel */}
+      {/* <Carousel /> */}
+      <div className="sm:mx-6 md:mx-10 lg:mx-12 px-3">
+      {/* Type */}
+      <Type />
+      {/* Rentals */}
+      <Rentals/>
+      </div>
+      {/* Footer */}
+      <Footer/>
     </div>
   );
 }
